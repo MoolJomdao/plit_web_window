6e05776ae15adca441a45a15d2779996725fc631
frenchblack <qkfqkfdl387@naver.com>
2018-10-02 02:56:57 +0900

    //DAO 수정

--- src/control/DAO.java
+++ src/control/DAO.java
@@ -1328,7 +1328,7 @@ public class DAO
     			}  			
     			jtmp.put("category",rs.getInt(9));
     			jtmp.put("comment_cnt",rs.getInt(10));
-    			jtmp.put("nickname",rs.getString(7));
+    			jtmp.put("nickname",rs.getString(11));
     			jrr.put(jtmp);
     			count++;
     			
@@ -1407,7 +1407,7 @@ public class DAO
     			}
     			jtmp.put("category",rs.getInt(9));
     			jtmp.put("comment_cnt",rs.getInt(10));
-    			jtmp.put("nickname",rs.getString(7));
+    			jtmp.put("nickname",rs.getString(11));
     			jrr.put(jtmp);
     			count++;
     			
